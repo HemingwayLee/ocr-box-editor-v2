6a0027ed911cc08a4cc5277e1f8b77a521377131
LeeHemingway <coherence0815@gmail.com>
2020-08-15 23:50:21 +0900

    add comment

--- scripts/generator.sh
+++ scripts/generator.sh
@@ -3,6 +3,7 @@
 path=$1
 lng=$2
 
+# it needs to be tesseract 4.1.1 or higher version, 4.0.0 does not support wordstrbox
 boxfilename=$(echo "$path" | cut -f 1 -d '.')
 tesseract $path $boxfilename -l $lng --psm 6 wordstrbox
 
